3a97b48525e7607586bca415f098ab66496ba4ba
Kamesh Madduri <kamesh.madduri@gmail.com>
2019-03-07 10:58:11 -0500

    updating bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+# set up Eigen
+curl -O https://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
+tar -zvxf 3.3.7.tar.gz
+mv eigen-eigen-*/Eigen .
+rm -rf eigen-eigen-* 3.3.7.tar.gz
+
+# get the lode PNG encoder
+curl -O https://github.com/lvandeve/lodepng/blob/master/lodepng.h
+curl -O https://github.com/lvandeve/lodepng/blob/master/lodepng.cpp
+mv lodepng.cpp lodepng.c
